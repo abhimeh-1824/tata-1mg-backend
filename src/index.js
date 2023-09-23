641c2755aa3424fd81c8186b1f5bd3304e5ffb44
ABHISHEK Mehra <abhishekmehra1245@gmail.com>
2023-09-23 13:44:36 +0530

    update mongod db url

--- src/index.js
+++ src/index.js
@@ -41,7 +41,6 @@ app.use("/healthFood", healthFoodController);
 // -------------
 app.use("/zandu", zanduController);
 app.use("/zandu/id", zanduController);
-
 app.use("/brand", BrandsController)
 app.use("/item", ItemsController)
 app.use("/uses", UsesController)
@@ -49,7 +48,8 @@ app.use("/productForm", ProductFormController)
 
 app.get("/", async (req, res) => {
     res.status(200).send({
-        demo: 'true'
+        demo: 'true',
+        newSchema: 'true'
     })
 })
 
